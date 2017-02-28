1576e3e34f64d535a19ce1f326df41367e01ab7f
unknown <heart2heart.it@gmail.com>
2017-02-28 23:18:56 +0800

    update follow the vnthuquan changes

--- vnthuquan.py
+++ vnthuquan.py
@@ -1,30 +1,33 @@
 #-*- encoding: utf-8 -*-
-import httplib
+import requests
 import re
 from pyquery import PyQuery as pq
 import unicodedata
 import sys
+
+domain = 'http://vnthuquan.org'
+
 def fetch(url):
 	headers = {'Cookie':'ASP.NET_SessionId=xx'}
-	conn = httplib.HTTPConnection("vnthuquan.net")
-	conn.request("GET", url, "", headers)
-	res = conn.getresponse()
-	print res.status, res.reason
-	data = res.read()
-	conn.close()
-	chap_titles = re.findall(r'class="normal8">(.+?)</a>', data)
+	res = requests.get(domain + url, headers=headers)
+	print res.status_code, res.reason
+	data = res.text
+	chap_titles = re.findall(r'">(.+?)</acronym>', data)
+	print len(chap_titles), chap_titles
 	len_story = len(chap_titles)
-	chap_descript = re.findall(r'title="(.+?)"><div', data)
-	tuaid = re.findall(r'thong_so\+="(\d+?)"', data)[0]
+	chap_descript = re.findall(r'<acronym title="(.+?)"><a', data)
+	print len(chap_descript), chap_descript
+	chap_links = re.findall(r'"><a href="(.+?)">', data)[:-2]
+	print len(chap_links), chap_links
 	d = pq(data)
 	title = d('title').text()
-	f = open(re.sub('[^A-Za-z0-9 \-,]+', '', unicodedata.normalize('NFKD', title).encode('ascii','ignore')) + '.html', 'w')
+	f = open(re.sub('[^A-Za-z0-9 \-,]+', '', unicodedata.normalize('NFKD', title).encode('ascii','ignore')) + '.html', 'wb')
 	if len_story:
 		f.write('<body><table style="width:100%">')
 		n = 0
 		for x,y in zip(chap_titles,chap_descript):
 			#x = x.html()
-			f.write('<tr><td><a href="#phan' + str(n) + '">'+ x + ' - '  + y + '</a></td></tr>')
+			f.write('<tr><td><a href="#phan' + str(n) + '">'+ x[69:-4].encode('utf8') + ' - '  + y.encode('utf8') + '</a></td></tr>')
 			n += 1
 		f.write('</table>')
 	else:
@@ -34,22 +37,18 @@ def fetch(url):
 		len_story = 1
 	n = 0
 	for ch_id in xrange(len_story):
-		link = '/truyen/chuonghoi_moi.aspx?&rand=415.9540610185356'
-		ch_link = 'tuaid=' + tuaid + '&chuongid=' + str(ch_id+1)
+		link = chap_links[ch_id].replace('truyen.aspx?tid=', 'http://vnthuquan.org/truyen/chuongtext.aspx?tid=') + '&rand=267.3781815'
 		headers = {'Cookie':'ASP.NET_SessionId=xx', 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}
-		conn = httplib.HTTPConnection("vnthuquan.net")
-		conn.request("POST", link, ch_link, headers)
-		res = conn.getresponse()
-		print res.status, res.reason
-		data = res.read()
-		conn.close()
+		res = requests.get( link, headers=headers)
+		print res.status_code, res.reason
+		data = res.text
 		data = data.split('--!!tach_noi_dung!!--')[1:]
-		x = 'class="tieude0anh" id="phan' + str(n) + '"'
-		f.write(data[0].replace('class="tieude0anh"', x) + data[1])
+		x = 'p align="center" class="style26" id="phan' + str(n) + '"'
+		f.write(data[0].replace('p align="center" class="style26"', x).encode('utf8') + data[1].encode('utf8'))
 		n += 1
 	f.write('</body>')
 	f.close()
 
 	
-link = sys.argv[1].replace('http://vnthuquan.net', '')
+link = sys.argv[1].replace(domain, '')
 fetch(link)
